0adf03c29593ac555deb765fc13a744700383495
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-06-16 16:28:22 -0400

    refactoring to Controller Classes

--- simple-site/vendor/composer/autoload_static.php
+++ simple-site/vendor/composer/autoload_static.php
@@ -12,9 +12,11 @@ class ComposerStaticInitc2b724d629978b8597fff8bd807a6a15
         'Composer\\Autoload\\ClassLoader' => __DIR__ . '/..' . '/composer/ClassLoader.php',
         'Composer\\Autoload\\ComposerStaticInitc2b724d629978b8597fff8bd807a6a15' => __DIR__ . '/..' . '/composer/autoload_static.php',
         'Connection' => __DIR__ . '/../..' . '/core/database/Connection.php',
+        'PagesController' => __DIR__ . '/../..' . '/controllers/PagesController.php',
         'QueryBuilder' => __DIR__ . '/../..' . '/core/database/QueryBuilder.php',
         'Request' => __DIR__ . '/../..' . '/core/Request.php',
         'Router' => __DIR__ . '/../..' . '/core/Router.php',
+        'UsersController' => __DIR__ . '/../..' . '/controllers/UsersController.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
--- simple-site/views/users.view.php
+++ simple-site/views/users.view.php
@@ -1,21 +1,19 @@
 <?php require('partials/head.php'); ?>
 
-<h1>Home Page</h1>
-
-<!-- <h1>Submit your name</h1>
-
-<form method='POST' action='/names'>
-  <input name='name'>
-  <button type="submit">Submit</button>
-</form>
-
-<h2>Names</h2>
+<h2>All Users</h2>
 <ul>
   <?php foreach ($users as $user): ?>
   <li>
     <?= $user->name ?>
   </li>
   <?php endforeach; ?>
-</ul> -->
+</ul>
+
+<h1>Submit your name</h1>
+
+<form method='POST' action='/users'>
+  <input name='name'>
+  <button type="submit">Submit</button>
+</form>
 
 <?php require('partials/footer.php'); ?>
