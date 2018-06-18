f1d178287e94817a2d2c119be627e9f94f77931e
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-06-18 14:30:01 -0400

    implemented namespacing and moved mvc into app direcory

--- simple-site/app/controllers/PagesController.php
+++ simple-site/app/controllers/PagesController.php
@@ -1,18 +1,18 @@
 <?php
 
+namespace App\Controllers;
+
 class PagesController
 {
     public function home()
     {
-        $users = App::get('database')->selectAll('users');
-
         return view('index', compact('users'));
     }
 
     public function about()
     {
         $company = 'Laracasts';
-        
+
         return view('about', compact('company'));
     }
 
--- simple-site/vendor/composer/autoload_classmap.php
+++ simple-site/vendor/composer/autoload_classmap.php
@@ -6,14 +6,15 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'App' => $baseDir . '/core/App.php',
+    'App\\Controllers\\PagesController' => $baseDir . '/app/controllers/PagesController.php',
+    'App\\Controllers\\UsersController' => $baseDir . '/app/controllers/UsersController.php',
+    'App\\Core\\App' => $baseDir . '/core/App.php',
+    'App\\Core\\Request' => $baseDir . '/core/Request.php',
+    'App\\Core\\Router' => $baseDir . '/core/Router.php',
+    'App\\Models\\Project' => $baseDir . '/app/models/Project.php',
     'ComposerAutoloaderInitc2b724d629978b8597fff8bd807a6a15' => $vendorDir . '/composer/autoload_real.php',
     'Composer\\Autoload\\ClassLoader' => $vendorDir . '/composer/ClassLoader.php',
     'Composer\\Autoload\\ComposerStaticInitc2b724d629978b8597fff8bd807a6a15' => $vendorDir . '/composer/autoload_static.php',
     'Connection' => $baseDir . '/core/database/Connection.php',
-    'PagesController' => $baseDir . '/controllers/PagesController.php',
     'QueryBuilder' => $baseDir . '/core/database/QueryBuilder.php',
-    'Request' => $baseDir . '/core/Request.php',
-    'Router' => $baseDir . '/core/Router.php',
-    'UsersController' => $baseDir . '/controllers/UsersController.php',
 );
